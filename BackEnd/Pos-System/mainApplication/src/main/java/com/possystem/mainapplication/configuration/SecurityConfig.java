efd2ae48526ba3732a9193bb42ce14d72fcd7510
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-15 11:58:10 +0530

    security filter chain is configured and added cors config and

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/configuration/SecurityConfig.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/configuration/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.possystem.mainapplication.configuration;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.jspecify.annotations.Nullable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,8 +9,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 @EnableWebSecurity
 @Configuration
@@ -22,29 +29,80 @@ public class SecurityConfig {
         String publicApis[] = {"/test"};
 
 
+        String adminUrls[] = {"/api/**"};
+        String superAdminUrls[] = {"/api/super-admin/**"};
+
+
         httpSecurity
-//                disable csrf
-                .csrf(csrf -> csrf.disable())
-//                disable cors
-                .cors(cors -> cors.disable())
+
 
 // creating session management state is stateless , here we use JWT token
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                no auth is required for publicApis so rest of apis requires auth
+//                no auth is required for publicApis so rest of apis requires auth suppose if we use  .anyRequest().permitAll()
                 .authorizeHttpRequests(authorise -> authorise
 //                           no auth is required for publicApis so rest of apis requires auth
                         .requestMatchers(publicApis).permitAll()
 
+//                        to access admin urls we have to authenticate
+                        .requestMatchers(adminUrls).authenticated()
+//                        to access this urls user must have a role admin and must be authneticated
+                        .requestMatchers(superAdminUrls).hasRole("ADMIN")
 
-
-                        .anyRequest().authenticated());
-
+//                        rest of urls no need to authenticate
+                        .anyRequest().permitAll())
+//  add filter
+                .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
+                // disable csrf
+                .csrf(csrf -> csrf.disable())
+//                disable cors
+                .cors(cors -> cors.configurationSource(corsConfigurationMethod()));
 
 //                default form login
 //                .formLogin(Customizer.withDefaults());
 
 
         return httpSecurity.build();
+    }
+
+    //    here we configure cors and we allowed required origin domain urls
+    private CorsConfigurationSource corsConfigurationMethod() {
+
+//        return  new CorsConfigurationSource() {
+//            @Override
+//            public @Nullable CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//                return null;
+//            }
+//        };
+
+        CorsConfigurationSource AllowedDomains = new CorsConfigurationSource() {
+            //            annominous function without functional interface
+            @Override
+            public @Nullable CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+// here we configure cors
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(
+                        // here we add frontend urls and third party urls
+                        Arrays.asList("http://localhost:5173/", "http://localhost:3000/"));
+//                here we set  allowed methods like fetch,post,put,delete
+                cfg.setAllowedMethods(Collections.singletonList("*")); // we allowed all methods
+//                here we allowed credemtials
+                cfg.setAllowCredentials(true);
+// here we set  headers
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+//                here we expose headers
+                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+
+                cfg.setMaxAge(3600L);
+
+                return cfg;
+            }
+        };
+
+
+        return AllowedDomains;
+
+
     }
 
 
