27062b2ff35147c53c29fd978aeb7fb8cea72c4b
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-19 08:55:51 +0530

    updates user modal class

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/UserModal.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/UserModal.java
@@ -1,16 +1,29 @@
 package com.possystem.mainapplication.modal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.*;
 
 @Data
 @Setter
 @Getter
-
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class UserModal {
+
+    @Id
+    private  Long ID;
+
+    @Column(nullable = false)
+    private  String fullName;
+
+    @Column(nullable = false,unique = true)
+    @Email(message = "Email should be valid...")
+    private String email;
+
 
 
 
