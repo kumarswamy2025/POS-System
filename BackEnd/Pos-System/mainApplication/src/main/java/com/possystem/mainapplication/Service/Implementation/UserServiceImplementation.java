fb46e51355e403ea7059a533c7162b1c07c31652
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-23 09:49:33 +0530

    implementing user service interface in userServiceImplemetation class

---
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Implementation/UserServiceImplementation.java
@@ -0,0 +1,36 @@
+package com.possystem.mainapplication.Service.Implementation;
+
+import com.possystem.mainapplication.Service.Services.UserService;
+import com.possystem.mainapplication.modal.UserModal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class UserServiceImplementation implements UserService {
+    @Override
+    public UserModal getUserFromJWTToken(String token) {
+        return null;
+    }
+
+    @Override
+    public UserModal getCurrentUser() {
+        return null;
+    }
+
+    @Override
+    public UserModal getUserByEmail(String Email) {
+        return null;
+    }
+
+    @Override
+    public UserModal getUserById(Long Id) {
+        return null;
+    }
+
+    @Override
+    public List<UserModal> getAllUsers() {
+        return List.of();
+    }
+}
