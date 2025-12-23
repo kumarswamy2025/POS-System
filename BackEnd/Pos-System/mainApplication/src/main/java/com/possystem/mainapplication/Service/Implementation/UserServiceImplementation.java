29cde0b947f73368b84a00e7e72f374865b34c3c
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-23 12:15:58 +0530

    implementing user service interface in userServiceImplemetation class

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Implementation/UserServiceImplementation.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Implementation/UserServiceImplementation.java
@@ -1,7 +1,9 @@
 package com.possystem.mainapplication.Service.Implementation;
 
 import com.possystem.mainapplication.Service.Services.UserService;
+import com.possystem.mainapplication.configuration.jwtConfig.JwtService;
 import com.possystem.mainapplication.modal.UserModal;
+import com.possystem.mainapplication.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +11,17 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImplementation implements UserService {
+
+//    here we inject dependencies NOTE: here we using @RequiredArgsConstructor annotations so no need to use constractor for dependency injection
+    private final UserRepo userRepo;
+    private final JwtService jwtService;
+
+
     @Override
     public UserModal getUserFromJWTToken(String token) {
+
+        String Email=jwtService.getEmailFromToken(token);
+
 
 
         return null;
