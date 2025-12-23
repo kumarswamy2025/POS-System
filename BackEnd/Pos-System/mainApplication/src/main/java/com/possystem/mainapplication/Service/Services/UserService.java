e70f8cd7ae19515966d61eb999e5fba8f8be8eda
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-23 09:45:28 +0530

    user service is setuped successfully

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/UserService.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/UserService.java
@@ -1,5 +1,14 @@
 package com.possystem.mainapplication.Service.Services;
 
+import com.possystem.mainapplication.modal.UserModal;
+
+import java.util.List;
+
 public interface UserService {
+    UserModal getUserFromJWTToken(String token);
+    UserModal getCurrentUser();
+    UserModal getUserByEmail(String Email);
+    UserModal getUserById(Long Id);
+    List<UserModal> getAllUsers();
 
 }
