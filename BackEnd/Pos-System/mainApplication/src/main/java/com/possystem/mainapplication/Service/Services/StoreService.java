b53cd50182196748c30324e2941f157b01ee33c9
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-24 18:37:39 +0530

    creating Store service interface

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/StoreService.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/StoreService.java
@@ -1,8 +1,12 @@
 package com.possystem.mainapplication.Service.Services;
 
+import com.possystem.mainapplication.modal.UserModal;
+import com.possystem.mainapplication.payload.DTO.StoreDTO;
 import org.springframework.stereotype.Service;
 
-public class StoreService {
+public interface StoreService {
+
+    StoreDTO createStore(StoreDTO storeDTO, UserModal userModal);
 
 
 }
