f52f19de45e05ec435982728e05d164b04236f48
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-24 18:14:20 +0530

    updated store Address class

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/StoreAddress.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/StoreAddress.java
@@ -1,9 +1,20 @@
 package com.possystem.mainapplication.modal;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Data
+@Getter
+@Setter
 @Embeddable
 public class StoreAddress {
+    private String address;
+    private  String phone;
+    @Email(message = "invalid email format")
+    private String email;
 
 
 }
