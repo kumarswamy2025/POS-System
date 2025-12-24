dfc3580d61ca58faaf891d1fbffc489fdbe71dec
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-24 19:18:34 +0530

    implemeting methods

---
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/repository/StoreRepo.java
@@ -0,0 +1,7 @@
+package com.possystem.mainapplication.repository;
+
+import com.possystem.mainapplication.modal.StoreModal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepo extends JpaRepository<StoreModal,Long> {
+}
