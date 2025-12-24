faaa8fcfccb07428d9d060df2e330f25797f5ffc
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-24 19:34:45 +0530

    inject dependenciess

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Implementation/StoreServiceImplementation.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Implementation/StoreServiceImplementation.java
@@ -15,8 +15,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class StoreServiceImplementation implements StoreService {
 
-    private UserRepo userRepo;
-    private StoreRepo storeRepo;
+    private final UserRepo userRepo;
+    private final StoreRepo storeRepo;
 
 
     @Override
