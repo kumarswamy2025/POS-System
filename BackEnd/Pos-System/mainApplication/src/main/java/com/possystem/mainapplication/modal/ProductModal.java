2a33c18fd6e0a430107696196d8cdf45725d11da
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-29 16:10:47 +0530

    product modal is created successfully

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/ProductModal.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/ProductModal.java
@@ -1,4 +1,52 @@
 package com.possystem.mainapplication.modal;
 
+import com.possystem.mainapplication.domain.StoreStatus;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
 public class ProductModal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String sku;
+    private String description;
+    private double mrp;
+    private double sellingPrice;
+    private String brand;
+    private String image;
+    //    category
+    @ManyToOne
+    private StoreModal storeModal;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // ✔️ Runs only once, when the entity is first persisted.
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+
+    }
+
+    // ✔️ Runs every time the entity is updated.
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+
 }
--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/StoreModal.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/StoreModal.java
@@ -50,5 +50,4 @@ public class StoreModal {
         updatedAt=LocalDateTime.now();
     }
 
-
 }
