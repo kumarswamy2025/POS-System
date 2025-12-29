3c654d3358dc172c365cc369713b883770c21195
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-29 16:26:26 +0530

    product service is successfully created

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/ProductService.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/ProductService.java
@@ -1,5 +1,22 @@
 package com.possystem.mainapplication.Service.Services;
 
+import com.possystem.mainapplication.modal.UserModal;
+import com.possystem.mainapplication.payload.DTO.ProductDTO;
+
+import java.util.List;
+
 public interface ProductService {
+
+    ProductDTO createProduct(ProductDTO productDTO, UserModal userModal);
+
+    ProductDTO updateProduct(Long id, ProductDTO productDTO, UserModal userModal);
+
+    void deleteProduct(Long id, UserModal userModal);
+
+    List<ProductDTO> getProductsByStoreId(Long storeId);
+
+    List<ProductDTO> searchByKeyword(Long storeId, String keyword);
+
+    ProductDTO getProductById(Long id, UserModal userModal);
 
 }
---
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/payload/DTO/ProductDTO.java
@@ -0,0 +1,31 @@
+package com.possystem.mainapplication.payload.DTO;
+
+import com.possystem.mainapplication.modal.StoreModal;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String sku;
+    private String description;
+    private double mrp;
+    private double sellingPrice;
+    private String brand;
+    private String image;
+    private Long categoryId;
+    private Long storeId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
