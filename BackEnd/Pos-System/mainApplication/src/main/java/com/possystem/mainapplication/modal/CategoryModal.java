56f5ab09fb4f3053d4a9026a50fc2318ff1a15bd
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-30 19:47:35 +0530

    successfully created catogory modal

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/CategoryModal.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/CategoryModal.java
@@ -1,6 +1,24 @@
 package com.possystem.mainapplication.modal;
 
+import jakarta.persistence.*;
+import lombok.*;
 
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
 public class CategoryModal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private StoreModal store;
 
 }
