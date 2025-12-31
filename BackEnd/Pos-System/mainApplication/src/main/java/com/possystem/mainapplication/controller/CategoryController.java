5ebf2f21f100037906cce1981ad0d9d3e57acbdc
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-12-31 08:18:02 +0530

    creating category controller

---
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/controller/CategoryController.java
@@ -0,0 +1,16 @@
+package com.possystem.mainapplication.controller;
+
+import com.possystem.mainapplication.Service.Services.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+
+}
