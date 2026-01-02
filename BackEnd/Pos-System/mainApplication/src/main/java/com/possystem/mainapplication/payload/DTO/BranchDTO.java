cc089cd28711455a86b96f57a12e1ab47094b432
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-01-02 11:14:58 +0530

    branch dto is created successfully

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/payload/DTO/BranchDTO.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/payload/DTO/BranchDTO.java
@@ -1,5 +1,34 @@
 package com.possystem.mainapplication.payload.DTO;
 
+import com.possystem.mainapplication.modal.StoreModal;
+import com.possystem.mainapplication.modal.UserModal;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+
+@Data
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BranchDTO {
+    private Long id;
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+    private List<String> workingDays;
+    private LocalTime openTime;
+    private LocalTime closeTime;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private StoreDTO store;
+    private String storeId;
+    private UserDTO managerId;
 
 }
