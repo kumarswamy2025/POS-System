eb535e46a6869d88d6810a7334b7398898dbd627
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-01-03 08:34:13 +0530

    branch service is created successfully

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/BranchService.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/BranchService.java
@@ -1,5 +1,19 @@
 package com.possystem.mainapplication.Service.Services;
 
+import com.possystem.mainapplication.modal.UserModal;
+import com.possystem.mainapplication.payload.DTO.BranchDTO;
+
+import java.util.List;
+
 public interface BranchService {
 
+    BranchDTO createBranch(BranchDTO branchDTO, UserModal userModal);
+
+    BranchDTO updateBranch(Long id, BranchDTO branchDTO, UserModal userModal);
+
+    BranchDTO getBranchById(Long id);
+
+    List<BranchDTO> getAllBranchesByStoreId(Long storeId);
+
+    void deleteBranch(Long id);
 }
