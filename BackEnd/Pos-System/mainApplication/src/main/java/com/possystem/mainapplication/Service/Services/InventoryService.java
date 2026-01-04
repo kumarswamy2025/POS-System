acf15a1319de841653899355ecfd23651023eaf7
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-01-04 15:18:57 +0530

    inventory service interface  is created successfully

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/InventoryService.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/InventoryService.java
@@ -1,5 +1,20 @@
 package com.possystem.mainapplication.Service.Services;
 
+import com.possystem.mainapplication.payload.DTO.InventoryDTO;
+
+import java.util.List;
+
 public interface InventoryService {
+
+    InventoryDTO createInventory(InventoryDTO inventoryDTO);
+    InventoryDTO updateInventory(Long id,InventoryDTO inventoryDTO);
+    void deleteInventory(Long id);
+    InventoryDTO getInventory(Long id);
+    List<InventoryDTO> getInventoryByProductId(Long productId);
+    List<InventoryDTO> getInventoryByBranchId(Long branchId);
+    InventoryDTO getInventoryByProductIdAndBranchId(Long productId,Long branchId);
+    List<InventoryDTO> getAllInventories();
+
+
 
 }
