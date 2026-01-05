b6c195a5a18d1c9ec4e4bc87bd8cbb613f914367
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-01-05 08:53:22 +0530

    successfully created employee service interface

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/EmployeeService.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/EmployeeService.java
@@ -1,4 +1,20 @@
 package com.possystem.mainapplication.Service.Services;
 
+import com.possystem.mainapplication.domain.UserRole;
+import com.possystem.mainapplication.modal.UserModal;
+import com.possystem.mainapplication.payload.DTO.UserDTO;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    UserDTO createEmployee(UserDTO userDTO,Long storeId);
+    UserDTO createBranchEmployee(UserDTO userDTO,Long branchId);
+    UserModal updateEmployee(Long employeeId,UserModal employeeDetails);
+    void  deleteEmployee(Long employeeId);
+    List<UserModal> findStoreEmployees(Long storeId, UserRole userRole);
+    List<UserModal> findBranchEmployees(Long branchId,UserRole userRole);
+    UserModal getEmployeeDetails(Long employeeId,UserRole userRole);
+
+
 }
