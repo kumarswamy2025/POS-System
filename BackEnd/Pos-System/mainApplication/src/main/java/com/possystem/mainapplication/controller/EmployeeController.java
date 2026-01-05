13b3bdf567ada84312e0639ea197f53c8cf4a656
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-01-05 18:57:48 +0530

    creating employee controller

---
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.possystem.mainapplication.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+}
