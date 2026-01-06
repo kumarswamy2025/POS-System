6d6fcfb743881a086da73a0c23dd7d51a1053bd2
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-01-06 09:20:13 +0530

    customer modal is created successfully

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/CustomerModal.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/CustomerModal.java
@@ -1,10 +1,10 @@
 package com.possystem.mainapplication.modal;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Data
 @Setter
@@ -17,4 +17,25 @@ public class CustomerModal {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(nullable = false)
+    private String firstName;
+
+    private String email;
+    private String phone;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // ✔️ Runs only once, when the entity is first persisted.
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+
+    }
+
+    // ✔️ Runs every time the entity is updated.
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
 }
