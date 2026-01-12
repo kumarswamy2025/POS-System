6aeabe72151af76d0704cb44ecc41306acfa01de
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-01-12 08:07:42 +0530

    order modal is created

---
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/OrderModal.java
@@ -0,0 +1,17 @@
+package com.possystem.mainapplication.modal;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class OrderModal {
+
+
+
+}
