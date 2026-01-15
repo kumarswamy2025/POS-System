8d898e77ff48955244e54206261bb9bf87be1aa6
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-01-15 09:27:55 +0530

    refund modal is created successfully

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/RefundModal.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/modal/RefundModal.java
@@ -1,4 +1,45 @@
 package com.possystem.mainapplication.modal;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.possystem.mainapplication.domain.PaymentType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
 public class RefundModal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private OrderModal orderModal;
+    private String reason;
+    private Double amount;
+    @ManyToOne
+    @JsonIgnore
+    private ShiftReportModal shiftReport;
+    @ManyToOne
+    private UserModal cashier;
+    @ManyToOne
+    private BranchModal branch;
+    private LocalDateTime createdAt;
+
+    private PaymentType paymentType;
+
+
+    // ✔️ Runs only once, when the entity is first persisted.
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+
+    }
+
+
 }
