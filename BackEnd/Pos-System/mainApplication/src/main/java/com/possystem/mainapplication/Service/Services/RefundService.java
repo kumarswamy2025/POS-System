aea4454cae1fd1f84330b7d762e3902e3ede4f76
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-01-16 09:27:06 +0530

    refund service is implemented successfully

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/RefundService.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/RefundService.java
@@ -1,4 +1,23 @@
 package com.possystem.mainapplication.Service.Services;
 
+import com.possystem.mainapplication.modal.RefundModal;
+import com.possystem.mainapplication.payload.DTO.RefundDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface RefundService {
+    RefundDTO createRefund(RefundModal refundModal);
+    List<RefundDTO> getAllRefunds();
+    RefundDTO getRefundByCashier(Long cashierId);
+    RefundDTO getRefundByShiftReport(Long shiftReportId);
+    List<RefundDTO> getRefundByCashierAndDateRange(Long cashierId, LocalDateTime startDate,LocalDateTime EndDate);
+
+    List<RefundDTO> getRefundByBranch(Long branchId);
+    RefundDTO getRefundById(Long refundID);
+    void deleteRefund(Long refundId);
+
+
+
+
 }
