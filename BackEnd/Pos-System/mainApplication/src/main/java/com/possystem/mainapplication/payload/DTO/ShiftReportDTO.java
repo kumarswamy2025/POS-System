0772741b17eec53e6653eaeac5048ba1dcd6936c
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-01-17 12:47:43 +0530

    implementing shift report classs

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/payload/DTO/ShiftReportDTO.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/payload/DTO/ShiftReportDTO.java
@@ -1,4 +1,39 @@
 package com.possystem.mainapplication.payload.DTO;
 
+import com.possystem.mainapplication.modal.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Transient;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Data
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ShiftReportDTO {
+    private Long id;
+
+    private LocalDateTime shiftStart;
+    private LocalDateTime shiftEnd;
+    private Double totalSale;
+    private Double totalRefund;
+    private Double netSale;
+    private int totalOrders;
+    @ManyToOne
+    private UserModal cashier;
+    @ManyToOne
+    private BranchModal branch;
+    @Transient
+    private List<PaymentSummary> PaymentSummaries;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<ProductModal> topSellingProducts;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderModal> recentOrders;
+    @OneToMany(mappedBy = "shiftReport",cascade = CascadeType.ALL)
+    private List<RefundModal> refunds;
 }
