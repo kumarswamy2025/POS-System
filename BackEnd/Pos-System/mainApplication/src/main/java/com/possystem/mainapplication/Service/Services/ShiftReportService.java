d600ab401509bbf9b8804193c35fb183710701e5
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-01-17 17:02:01 +0530

    added abstract methods in shift report service interface

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/ShiftReportService.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/Service/Services/ShiftReportService.java
@@ -1,5 +1,28 @@
 package com.possystem.mainapplication.Service.Services;
 
+import com.possystem.mainapplication.payload.DTO.ShiftReportDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ShiftReportService {
+
+    ShiftReportDTO startShift(Long cashierId, Long branchId, LocalDateTime startTime);
+
+    ShiftReportDTO endShift(Long shiftReportId,LocalDateTime shiftEnd);
+
+    ShiftReportDTO getShiftReportById(Long id);
+
+    List<ShiftReportDTO> getAllShiftReports();
+
+    List<ShiftReportDTO> getShiftReportsByBranchId(Long branchId);
+
+    List<ShiftReportDTO> getShiftReportsByCashierId(Long cashierId);
+
+    ShiftReportDTO getCurrentShiftProgress(Long cashierId);
+
+    ShiftReportDTO getShiftByCashierAndDate(Long cashierId,LocalDateTime date);
+
+
 
 }
