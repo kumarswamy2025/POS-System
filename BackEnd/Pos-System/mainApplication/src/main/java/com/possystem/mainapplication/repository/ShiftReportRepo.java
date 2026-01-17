955b997b0c00ef39028577c4e73a6e92504f195b
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-01-17 17:10:41 +0530

    shift repo is created and implemented successfully

--- BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/repository/ShiftReportRepo.java
+++ BackEnd/Pos-System/mainApplication/src/main/java/com/possystem/mainapplication/repository/ShiftReportRepo.java
@@ -1,7 +1,24 @@
 package com.possystem.mainapplication.repository;
 
 import com.possystem.mainapplication.modal.ShiftReportModal;
+import com.possystem.mainapplication.modal.UserModal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface ShiftReportRepo extends JpaRepository<ShiftReportModal,Long> {
+
+    List<ShiftReportModal> findByCashierId(Long cashierId);
+
+    List<ShiftReportModal> findByBranchId(Long id);
+
+
+    Optional<ShiftReportModal> findTopByCashierAndShiftEndIsNullOrderByShiftStartDesc(UserModal cashier);
+
+
+    Optional<ShiftReportModal> findByCashierAndShiftStartBetween(UserModal cashier, LocalDateTime start,LocalDateTime end);
+
+
 }
